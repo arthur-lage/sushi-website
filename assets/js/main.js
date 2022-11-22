c812cef4c48f97c5bbbfd4c4f257537ec04734da
Arthur Lage <arthurlage2006@gmail.com>
2022-11-21 22:58:14 -0300

    create dark theme colors, theme toggler, change link color depending on window offset y

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,25 +28,58 @@ navLinks.forEach((link) => link.addEventListener("click", linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 const scrollHeader = () => {
-  const header = document.querySelector("#header")
+  const header = document.querySelector("#header");
 
-  window.scrollY > 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header")
-}
+  window.scrollY > 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
 
-window.addEventListener("scroll", scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
 const scrollUp = () => {
-  const scrollUpButton = document.querySelector("#scrollup")
+  const scrollUpButton = document.querySelector("#scrollup");
 
-  window.scrollY > 350 ?  scrollUpButton.classList.add("show-scroll") : scrollUpButton.classList.remove("show-scroll")
-}
+  window.scrollY > 350
+    ? scrollUpButton.classList.add("show-scroll")
+    : scrollUpButton.classList.remove("show-scroll");
+};
 
-window.addEventListener("scroll", scrollUp)
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 50;
+    const sectionId = section.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      ".nav__menu a[href*=" + sectionId
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
+
 /*=============== DARK LIGHT THEME ===============*/
+
+const themeButton = document.querySelector("#theme-button");
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
